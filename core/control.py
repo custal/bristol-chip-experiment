b050511b0f4438c1b6463858b83fdc4032c5ff92
Chris Corlett <xl22293@bristol.ac.uk>
2022-11-08 19:19:11 +0000

    Add MockInstrument for testing purposes

--- core/control.py
+++ core/control.py
@@ -3,17 +3,25 @@
 from core.instruments import QuantifiManager, PowerMeterManager
 import numpy as np
 
-from core.utils import frequency_to_wavelength
-
 class ExperimentalSetUp:
     """ Class used to handle the experimental setup of a laser with a power meter """
 
-    def __init__(self, laser: LaserManager, power_meter: PowerMeterManager):
+    def __init__(self, laser: QuantifiManager, power_meter: PowerMeterManager):
 
         self.laser = laser
         self.power_meter = power_meter
 
     def perform_wavelength_sweep(self, wavelength_start: float, wavelength_end: float, step: float):
-        for wavelength in np.linspace(wavelength_start, wavelength_end, step):
+        for wavelength in np.arange(wavelength_start, wavelength_end, step):
             self.laser.set_wavelength(wavelength)
-            self.power_meter.take_measurement
+            self.power_meter.read()
+
+if __name__ == "__main__":
+    from core.utils import MockInstrument
+
+    laser = MockInstrument()
+    power_meter = MockInstrument()
+    setup = ExperimentalSetUp(laser, power_meter)
+
+    result = setup.perform_wavelength_sweep(1450, 1555, 0.5)
+    print(result)
--- core/utils.py
+++ core/utils.py
@@ -9,3 +9,51 @@ def frequency_to_wavelength(frequency: float, frequency_unit: str, wavelength_un
 
 def wavelength_to_frequency(wavelength: float, wavelength_unit: str, frequency_unit: str):
     return c / (wavelength * unit_conversion[frequency_unit] * unit_conversion[wavelength_unit])
+
+
+class MockInstrument:
+    """ This is a mock class used to test the analysis if not connected to live instruments """
+    def __init__(self):
+
+        self.state = False
+        self.wavelength = 1550
+        self.power = 7.5
+
+    def set_frequency(self, frequency: float):
+        self.wavelength = c/frequency
+
+    def shift_frequency(self, frequency_shift: float):
+        frequency = self.get_frequency()
+        self.set_frequency(frequency + frequency_shift)
+
+    def get_frequency(self):
+        return c/self.wavelength
+
+    def set_wavelength(self, wavelength: float):
+        self.wavelength = wavelength
+
+    def shift_wavelength(self, wavelength_shift: float):
+        wavelength = self.get_wavelength()
+        self.set_wavelength(wavelength + wavelength_shift)
+
+    def get_wavelength(self):
+        return self.wavelength
+
+    def set_power(self, power: float):
+        self.power = power
+
+    def shift_power(self, power_shift: float):
+        power = self.get_power()
+        self.set_power(power + power_shift)
+
+    def get_power(self):
+        return self.power
+
+    def set_state(self, state: bool):
+        self.state = state
+
+    def get_state(self):
+        return self.state
+
+    def read(self):
+        return self.power
