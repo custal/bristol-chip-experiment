60334d8fa691ac117dda8d03ba6cff8b70d47964
Chris Corlett <xl22293@bristol.ac.uk>
2022-11-15 15:05:21 +0000

    Add manuals for equipment to docs folder and create a create a TunicsManager class

--- core/instruments.py
+++ core/instruments.py
@@ -57,11 +57,11 @@ class InstrumentManager:
         return f"<{self._identify}, {self.instrument}>"
 
 
-class QuantifiManager(InstrumentManager):
+class TunicsManager(InstrumentManager):
     """
-        Wrapper class for managing the Quantifi laser
-        Quantifi laser SCPI commands can be found here:
-        https://cdn.quantifiphotonics.com/20220329110339/Laser_1000_Series_UserManual_V4p04.pdf
+        Wrapper class for managing the Tunics Plus laser
+        Tunics Plus laser SCPI commands can be found in the docs folder. The manual is for the Tunics T100S-HP not the
+        Tunics Plus but the commands should be similar.
     """
     def __init__(self, resource_name: str = 'TCPIP0::192.168.101.201::inst0::INSTR'):
         super().__init__(resource_name)
@@ -185,10 +185,136 @@ class QuantifiManager(InstrumentManager):
         return True if state == "ON" else False
 
 
+class QuantifiManager(InstrumentManager):
+    """
+        Wrapper class for managing the Quantifi laser
+        Quantifi laser SCPI commands can be found in the docs folder
+    """
+
+    def __init__(self, resource_name: str = 'TCPIP0::192.168.101.201::inst0::INSTR'):
+        super().__init__(resource_name)
+
+        if not isinstance(self.instrument, TCPIPInstrument):
+            raise TypeError(f"The instrument with name '{resource_name}' is not of type {TCPIPInstrument}.")
+
+        self.wavelength_unit = "NM"
+        self.frequency_unit = "THZ"
+        self.power_unit = "DBM"
+        self.resolution = 0.001  # WARNING: This is in nm and doesn't consider wavelength_unit
+
+        self.source = ":SOURCE1"
+        self.output = ":OUTP1"
+        self.channel = ":CHAN1"
+
+        self.set_power(7.5)  # default power is 10
+
+    @property
+    def source_prefix(self):
+        return self.source + self.channel
+
+    @property
+    def output_prefix(self):
+        return self.output + self.channel
+
+    def get_min_wavelength(self):
+        return float(self._send_message(f"{self.source_prefix}:WAV? MIN")) / unit_conversion[self.wavelength_unit]
+
+    def get_max_wavelength(self):
+        return float(self._send_message(f"{self.source_prefix}:WAV? MAX")) / unit_conversion[self.wavelength_unit]
+
+    def set_frequency(self, frequency: float):
+        if frequency < (min_freq := self.get_frequency("MIN")):
+            raise ValueError(f"Frequency '{frequency} {self.frequency_unit}' "
+                             f"is below laser minimum '{min_freq} {self.frequency_unit}'")
+        elif frequency > (max_freq := self.get_frequency("MAX")):
+            raise ValueError(f"Frequency '{frequency} {self.frequency_unit}' "
+                             f"is above laser maximum '{max_freq} {self.frequency_unit}'")
+
+        self._send_message(f"{self.source_prefix}:FREQ {frequency} {self.frequency_unit}", read=False)
+
+    def shift_frequency(self, frequency_shift: float):
+        frequency = self.get_frequency()
+        self.set_frequency(frequency + frequency_shift)
+
+    def get_frequency(self, param: str = ""):
+        """ param (str):
+                        MIN: Return the minimum programmable value
+                        MAX: Return the maximum programmable value
+                        DEF: Return the default value of frequency
+                        SET: Return the set value (default) of the frequency in the GRID
+                        ACT: Return the actual value of the SET frequency
+                        LOCK: Query whether the laser is currently at the SET frequency
+                        ALL: Returns all of the above parameters
+        ref manual pg. 51
+        """
+        return float(self._send_message(f"{self.source_prefix}:FREQ? {param}")) / unit_conversion[self.frequency_unit]
+
+    def set_wavelength(self, wavelength: float):
+        if wavelength < (min_wav := self.get_wavelength("MIN")):
+            raise ValueError(f"Wavelength '{wavelength} {self.wavelength_unit}' "
+                             f"is below laser minimum '{min_wav} {self.wavelength_unit}'")
+        elif wavelength > (max_wav := self.get_wavelength("MAX")):
+            raise ValueError(f"Wavelength '{wavelength} {self.wavelength_unit}' "
+                             f"is above laser maximum '{max_wav} {self.wavelength_unit}'")
+
+        self._send_message(f"{self.source_prefix}:WAV {wavelength} {self.wavelength_unit}", read=False)
+
+    def shift_wavelength(self, wavelength_shift: float):
+        wavelength = self.get_wavelength()
+        self.set_wavelength(wavelength + wavelength_shift)
+
+    def get_wavelength(self, param: str = ""):
+        """ param (str):
+                        MIN: Return the minimum programmable value
+                        MAX: Return the maximum programmable value
+                        DEF: Return the default value of wavelength
+                        SET: Return the set value (default) of the wavelength in the GRID
+                        ACT: Return the actual value of the SET wavelength
+                        LOCK: Query whether the laser is currently at the SET wavelength
+                        ALL: Returns all of the above parameters
+            ref manual pg. 51
+        """
+        return float(self._send_message(f"{self.source_prefix}:WAV? {param}")) / unit_conversion[self.wavelength_unit]
+
+    def set_power(self, power: float):
+        if power < (min_power := self.get_power("MIN")):
+            raise ValueError(f"Power '{power} {self.power_unit}' "
+                             f"is below laser minimum '{min_power} {self.power_unit}'")
+        elif power > (max_power := self.get_power("MAX")):
+            raise ValueError(f"Power '{power} {self.power_unit}' "
+                             f"is above laser maximum '{max_power} {self.power_unit}'")
+
+        self._send_message(f"{self.source_prefix}:POW {power} {self.power_unit}", read=False)
+        self.defined_power = power
+
+    def shift_power(self, power_shift: float):
+        power = self.get_power()
+        self.set_power(power + power_shift)
+
+    def get_power(self, param: str = ""):
+        """ param (str):
+                        MIN: Return the minimum programmable value
+                        MAX: Return the maximum programmable value
+                        DEF: Return the default value of power
+                        SET: Return the desired set value
+                        ACT: Return the current value (default)
+                        ALL: Returns all of the above parameters
+
+            ref manual pg. 50
+        """
+        return float(self._send_message(f"{self.source_prefix}:POW? {param}"))
+
+    def set_state(self, state: bool):
+        self._send_message(f"{self.output_prefix}:STATE {'ON' if state else 'OFF'}", read=False)
+
+    def get_state(self):
+        state = self._send_message(f"{self.output_prefix}:STATE?")
+        return True if state == "ON" else False
+
+
 class PowerMeterManager(InstrumentManager):
     """ Wrapper class for managing power meter instruments
-        Thorlab power meter SCPI commands can be found here:
-        https://www.thorlabs.com/drawings/3c723420dbbe302b-628E3525-E1CC-62FF-6BDFDA05526B9FE3/PM100D-Manual.pdf
+        Thorlab power meter SCPI commands can be found in the docs folder
     """
     def __init__(self, resource_name: str = 'USB0::0x1313::0x8078::P0010441::INSTR'):
         super().__init__(resource_name)
