14ecbfb62b93651d63472e37b47e72f8fab8a0a6
Ryan Tiew <h1010134@gmail.com>
2022-11-16 17:07:50 +0000

    add sweep graph; make folder

--- core/analysis.py
+++ core/analysis.py
@@ -24,3 +24,24 @@ g = sns.relplot(x = x, y = data)
 
 
 # %%
+import numpy as np
+import seaborn as sns
+import pandas as pd
+
+
+# %%
+#Plotting the sweep of the QUANTIFI laser
+fname = r"../Data/2022-11-16_laser_sweep_powersamples100_pm_sensitivity_1550000_QUANTIFI_powermeter_characterisation.txt"
+data = pd.read_csv(fname,index_col = 0)
+data["mean_dbm"] = data.mean(axis = 1)
+data["variance"] = data.var(axis = 1)
+data["std"]=np.sqrt(data["variance"])
+data.head()
+data.reset_index()
+data.head()
+data["Wavelength(nm)"] = data.index
+sns_plot = sns.relplot(data=data,x="Wavelength(nm)", y = "mean_dbm")
+fig = sns_plot.fig
+fig.savefig("../Graphs/QUANTIFI_laser_sweep_1550_sensitivity",dpi=600)
+# %%
+data["variance"]
